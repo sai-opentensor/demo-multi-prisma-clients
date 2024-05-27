97742182da92f4500c1a373aa3cf0846792483ba
skiran017 <sai@opentensor.dev>
2024-05-27 07:12:18 +0530

    update turbo.json

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ['@repo/ui'],
-  transpilePackages: ['@repo/database'],
+  // transpilePackages: ['@repo/database'],
 };
